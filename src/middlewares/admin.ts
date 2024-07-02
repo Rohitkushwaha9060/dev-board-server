034497d635d2e5b0bd05a7cf002d0b588142fbfe
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-07-02 18:33:14 +0530

    add admin middleware

---
+++ src/middlewares/admin.ts
@@ -0,0 +1,18 @@
+import { HttpError } from '@/errors';
+import { Request, Response, NextFunction } from 'express';
+
+export const checkAdminMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (!req.user) {
+        return next(new HttpError('Unauthorized', 401));
+    }
+
+    if (req.user.role !== 'admin') {
+        return next(new HttpError('Access denied', 401));
+    }
+
+    return next();
+};
